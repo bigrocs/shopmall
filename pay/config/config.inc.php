5d38a048fab64cf60773bcc56149b58f4f0be20e
bigrocs <bigrocs@qq.com>
2016-09-08 16:46:10 +0800

    安装支付后的文件提交一次

--- pay/config/config.inc.php
+++ pay/config/config.inc.php
@@ -2,8 +2,8 @@
 		$config['dbhost'] = 'localhost';      //数据库所在IP地址
 		$config['dbport'] = '';  //数据库用户
 		$config['dbuser'] = 'root';  //数据库用户
-		$config['dbpass'] = '123';   	 //数据库密码
+		$config['dbpass'] = 'root';   	 //数据库密码
 		$config['dbname'] = 'mallbuilder';     //数据库名
 		$config['table_pre']='pay_';  //数据库表前缀
-		$config['authkey']='831fbae75c31c2cb643ef3801570cf40';  //数据库表前缀
+		$config['authkey']='81864ef21de88f0b7049ce0cde1d8211';  //数据库表前缀
 		?>
